6e9f655848e8f49b04071d034f3a612bc7ed832a
m1shan <mkhl.davidenko@gmail.com>
2019-08-01 12:46:55 -0700

    some fixes

--- cmd/run_buildkite_agent.sh
+++ cmd/run_buildkite_agent.sh
@@ -4,6 +4,7 @@ echo $BUILDKITE_AGENT_TOKEN
 docker run \
     -v "/Users/mdavidenko/buildkite-secrets:/buildkite-secrets:ro" \
     -v "/Users/mdavidenko/buildkite-hooks/environment:/buildkite/hooks/environment" \
+    -v "/var/run/docker.sock:/var/run/docker.sock" \
     -d \
     -t \
     --name buildkite-agent \
